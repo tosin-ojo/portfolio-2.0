c6706241483132909f0fd161b17b4a3706fe5303
Tosin Ojo <tosinojo1996@gmail.com>
2021-12-09 22:36:38 +0100

    message api link

--- componets/Contact.js
+++ componets/Contact.js
@@ -33,7 +33,7 @@ const Contact = () => {
     setLoading(true);
 
     try {
-      await axios.post("http://localhost:5000/send", {
+      await axios.post("https://send-me-message.herokuapp.com/send", {
         name,
         email,
         message,
