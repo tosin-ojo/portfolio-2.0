b668d8ff235137d6573c0819ff4ac254dd172347
Tosin Ojo <tosinojo1996@gmail.com>
2022-01-01 21:24:25 +0100

    keywords

--- data/meta.js
+++ data/meta.js
@@ -3,7 +3,7 @@ export const Meta = {
   description:
     "Oluwatosin Ojo is a software engineer specializing in building web solutions. Keen on providing high-performing and user-friendly products.",
   keywords:
-    "Oluwatosin, Tosin, Ojo, Software, Full-Stack, Frontend, Backend, Developer, Engineer, Programmer, Coder, Nigeria, Africa, Javascript, ReactJs, NodeJs, NextJs",
+    "Oluwatosin, Tosin, Ojo, Software, Full-Stack, Frontend, Backend, Developer, Engineer, Programmer, Coder, Nigeria, Africa, Javascript, ReactJs, NodeJs, NextJs, MERN",
   image: "https://tosinojo.com/og.png",
   url: "https://tosinojo.com/",
   themeColor: "#0a192f",
