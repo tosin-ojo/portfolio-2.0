33965484223d8d672fc7007b5c90818d8b466300
Tosin Ojo <tosinojo1996@gmail.com>
2022-01-01 21:12:25 +0100

    devices compatability

--- componets/Footer.js
+++ componets/Footer.js
@@ -1,32 +1,22 @@
-import Github from '../icons/github'
-import Linkedin from '../icons/linkedin'
-import FreeCodeCamp from '../icons/freecodecamp'
-import Twitter from '../icons/twitter'
-import CodePen from '../icons/codepen'
-import Mail from '../icons/mail'
-import Whatsapp from '../icons/whatsapp'
+import { Name } from "../data/hero";
 
-import { Name } from '../data/hero'
+import { Contacts } from "../data/contacts";
 
-import { Contacts } from '../data/contacts'
-
-import styles from '../styles/Footer.module.css'
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
-    return (
-        <footer className={styles.footer}>
-            <div>
-                {Name}
-            </div>
-            <div>
-                {Contacts.map((contact) => 
-                    <a key={contact.name} href={contact.link} target="_blank">
-                        {contact.icon}
-                    </a>
-                )}
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className={styles.footer}>
+      <div>{Name}</div>
+      <div>
+        {Contacts.map((contact) => (
+          <a key={contact.name} href={contact.link} target="_blank">
+            {contact.icon}
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
