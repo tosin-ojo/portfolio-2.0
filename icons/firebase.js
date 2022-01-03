f6625725a5ddf9348f8ed9c1b67d8074328e3162
Tosin Ojo <tosinojo1996@gmail.com>
2022-01-03 22:54:14 +0100

    tech icons outlined

--- icons/firebase.js
+++ icons/firebase.js
@@ -1,7 +1,20 @@
 const firebase = () => {
-    return (
-        <svg fill="currentColor" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><path d="M3.89 15.672 6.255.461A.542.542 0 0 1 7.27.288l2.543 4.771zm16.794 3.692-2.25-14a.54.54 0 0 0-.919-.295L3.316 19.365l7.856 4.427a1.621 1.621 0 0 0 1.588 0zM14.3 7.147l-1.82-3.482a.542.542 0 0 0-.96 0L3.53 17.984z"/></svg>
-    )
-}
+  return (
+    <svg
+      fill="currentColor"
+      width="15px"
+      height="15px"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.5 11.5L11.5 3.5L12.5 12.5L7.5 14.5L2.5 11.5ZM2.5 11.5L7.5 2.5L9.5 5.5M2.5 11.5L3.5 0.5L6.5 3.5"
+        stroke="currentColor"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
 
-export default firebase
+export default firebase;
