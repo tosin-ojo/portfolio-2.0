3319e2b5367d59456b26391410feb74b954e2062
Tosin Ojo <ojo.oluwatosin.adebayo@gmail.com>
2025-07-07 15:39:01 +0100

    Update projects.tsx

--- data/projects.tsx
+++ data/projects.tsx
@@ -6,7 +6,7 @@ export const featured = [
       "Findexterous is a versatile financial management app offering a wide array of features, including expense monitoring, collaborative finance management, invoicing, payroll management, bookkeeping, and more, for streamlined financial operations.",
     technologies: ["NextJs", "RTK", "NodeJS", "MongoDB"],
     image: "/findexterous.webp",
-    link: "https://findex-seven.vercel.app/findex",
+    link: "https://findex-seven.vercel.app/",
     source: "",
   },
   {
